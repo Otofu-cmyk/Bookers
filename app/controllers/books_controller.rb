bb104a6d05a1a7fbf3b10bf4fcced4691ea64f50
otofu2713@gmail.com <ec2-user@ip-172-31-41-146.ec2.internal>
2020-12-18 01:34:06 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,24 +4,44 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book=Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book=Book.new
-  end
+  # def new
+  #   @book=Book.new
+  # end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    # 後ほどリンク先を書き換える
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      render action: :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
